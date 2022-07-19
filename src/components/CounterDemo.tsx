84b3c3d41fa5242b23943e496db13a11c3de5230
Melissa Rose <123melissa321@gmail.com>
2022-07-19 11:08:40 +0100

    Fixed counter

--- src/components/CounterDemo.tsx
+++ src/components/CounterDemo.tsx
@@ -4,19 +4,35 @@ function CounterDemo(): JSX.Element {
   const [counterValueFromCurrentRender, queueRerenderWithNewCounterValue] =
     useState(0);
 
+  const handleMinusFive = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 5);
+  };
+  
+  const handleMinusOne = () => {
+    queueRerenderWithNewCounterValue(counterValueFromCurrentRender - 1);
+  };
+
   const handleAddOne = () => {
     queueRerenderWithNewCounterValue(counterValueFromCurrentRender + 1);
   };
-
+const handleAddFive = () => {
+    queueRerenderWithNewCounterValue(previousValue => previousValue + 5);
+  };
   const handleReset = () => {
-    // TODO reset counter to 0 here
+    queueRerenderWithNewCounterValue(0);
   }
 
   return (
     <>
       <h1>Counter Demo</h1>
       <p>Current value: {counterValueFromCurrentRender}</p>
+      <button onClick={handleMinusFive}>-5</button>
+      <hr />
+      <button onClick={handleMinusOne}>-1</button>
+      <hr />
       <button onClick={handleAddOne}>+1</button>
+      <hr />
+      <button onClick={handleAddFive}>+5</button>
       <hr />
       <button onClick={handleReset}>Reset counter</button>
     </>
